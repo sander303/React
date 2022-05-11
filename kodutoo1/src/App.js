c294a3cfa2e827f5adc1ac990564b9bdaaf621ce
sander303 <105287127+sander303@users.noreply.github.com>
2022-05-11 16:34:36 +0300

    Update App.js

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <button>Kodutoo1</button>
       <p>See on sinine tekst</p>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" alt="" />
     </div>
   );
 }
