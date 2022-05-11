ced6a53d4ca4b887f6e570420d7bf54e7485957a
sander303 <105287127+sander303@users.noreply.github.com>
2022-05-11 16:33:23 +0300

    Kodutöö 1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Kodutoo1</button>
+      <p>See on sinine tekst</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" />
+    </div>
+  );
+}
+
+export default App;
